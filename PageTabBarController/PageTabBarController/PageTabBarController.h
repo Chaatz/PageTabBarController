d38eda3d74bbb90684f13c0eea5a3c30f08fcb02
Keith Chan <keith.chan@chaatz.com>
2017-03-13 12:28:06 +0800

    project setup

---
+++ PageTabBarController/PageTabBarController/PageTabBarController.h
@@ -0,0 +1,19 @@
+//
+//  PageTabBarController.h
+//  PageTabBarController
+//
+//  Created by Mingloan Chan on 3/9/17.
+//  Copyright © 2017 playground. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PageTabBarController.
+FOUNDATION_EXPORT double PageTabBarControllerVersionNumber;
+
+//! Project version string for PageTabBarController.
+FOUNDATION_EXPORT const unsigned char PageTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PageTabBarController/PublicHeader.h>
+
+
